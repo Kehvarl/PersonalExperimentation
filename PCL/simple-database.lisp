d027b14375deadadd1bbab0ae342970da6f84f3a
Kehvarl <Kehvarl@Kehvarl.com>
2020-06-02 05:19:58 -0700

    Naive implementation of prompt-for-cd

--- PCL/simple-database.lisp
+++ PCL/simple-database.lisp
@@ -10,3 +10,15 @@
 (defun dump-db ()
   (dolist (cd *db*)
     (format t "~{~a:~10t~a~%~}~%" cd)))
+
+(defun prompt-read (prompt)
+  (format *query-io* "~a: " prompt)
+  (force-output *query-io*)
+  (read-line *query-io*))
+
+(defun prompt-for-cd ()
+  (make-cd
+   (prompt-read "Title")
+   (prompt-read "Artist")
+   (prompt-read "Rating")
+   (prompt-read "Ripped [y/n]")))
