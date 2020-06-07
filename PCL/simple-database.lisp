abf727dfc7343248f3bf48462b00b034506e9c92
Kehvarl <Kehvarl@Kehvarl.com>
2020-06-06 18:22:11 -0700

    Update the CD library...

--- PCL/simple-database.lisp
+++ PCL/simple-database.lisp
@@ -54,6 +54,14 @@
        (if ripped-p (equal (getf cd :ripped) ripped) t)	   
        )))
 
-(defun select-by-artist (artist)
-  (select
-   (where :artist artist)))
+(defun update (selector-fn &key title artist rating (ripped nil ripped-p))
+  (setf *db*
+	(mapcar
+	 #'(lambda (row)
+	     (when (funcall selector-fn row)
+	       (if title    (setf (getf row :title) title))
+	       (if artist   (setf (getf row :artist) artist))
+	       (if rating   (setf (getf row :rating) rating))
+	       (if ripped-p (setf (getf row :ripped) ripped)))
+	     row)
+	 *db*)))
