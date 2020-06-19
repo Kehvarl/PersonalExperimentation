1ffefc1efd3b3efcda536153f86b7dff23f66b92
Kehvarl <Kehvarl@Kehvarl.com>
2020-06-19 16:47:43 -0700

    Create spam-filter project and start roughing in needed methods

---
+++ PCL/spam-filter.lisp
@@ -0,0 +1,14 @@
+(in-package :com.kehvarl.spam)
+
+
+(defun classify (text)
+  (classification (score (extract-features text))))
+
+(defparameter *max-ham-score* .4)
+(defparameter *min-spam-score* .6)
+
+(defun classification (score)
+  (cond
+    ((<= score *max-ham-score*) 'ham)
+    ((>= score *min-spam-score*) 'spam)
+    (t 'unsure)))
